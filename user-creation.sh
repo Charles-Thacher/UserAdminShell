e207bee52acf45e91e0fe1f720185314dbc403ff
Charles Thacher <crichardthacher@gmail.com>
2025-04-27 13:13:58 -0400

    created the user-creation script; untested

--- user-creation.sh
+++ user-creation.sh
@@ -1,10 +1,52 @@
+#!/bin/bash
+
 # 1. Create a new user
-read -p "Enter new username: " username
-echo "Checking if $username exists."
+# prompt username creation
 
-if 
+choose_username
 
-useradd $username
+function choose_username {
+    read -p "Enter new username: " username
+    check_for_existence
+}
+
+
+function check_for_existence {
+    # This function checks if a user exists and creates it if it doesn't
+
+    echo "Checking if $username exists."
+    
+    if grep -q "^${username}:" /etc/passwd; then
+        echo "$username is not available."
+            read -p "Do you want to proceed? (Y/N): " answer
+    case "$answer" in
+        [Yy]*) 
+            do_something
+            ;;
+        [Nn]*) 
+            echo "Exiting."
+            exit 0
+            ;;
+        *) 
+            echo "Invalid response. Exiting."
+            exit 1
+            ;;
+    esac
+
+    else
+        echo "$username is available. Creating user $username."
+        useradd "$username"
+        if grep -q "^${username}:" /etc/passwd; then
+            echo "$username successfully created!"
+        else
+            echo "Failed to create account for username "$username.\nScript failed.\n\nExiting."
+
+    fi
+}
 
 
 # 2. Initial configuration of user
+
+function select_password {
+
+}
