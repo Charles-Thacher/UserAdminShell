87f2240bf87b801fcb3855c37a23e78026af78ac
Charles Thacher <crichardthacher@gmail.com>
2025-04-28 00:15:51 -0400

    added code to the user-creation shell

--- user-creation.sh
+++ user-creation.sh
@@ -1,52 +1,77 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Found online as a snippet. Keeps script from being run without sudo privalages.
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (use sudo)"
+  exit 1
+fi
+
+
 
 # 1. Create a new user
 # prompt username creation
 
-choose_username
-
-function choose_username {
-    read -p "Enter new username: " username
-    check_for_existence
-}
-
-
-function check_for_existence {
+function create_user {
     # This function checks if a user exists and creates it if it doesn't
+
+    read -p "Enter new username: " username
+
+    echo "Checking if $username is valid."
+
+    # ensure username only contains valid alphanumerica characters
+    if [[ ! "$username" =~ ^[a-zA-z0-9_]+$ ]]; then
+        
+
+    else
+        
+    fi
 
     echo "Checking if $username exists."
     
     if grep -q "^${username}:" /etc/passwd; then
         echo "$username is not available."
             read -p "Do you want to proceed? (Y/N): " answer
-    case "$answer" in
-        [Yy]*) 
-            do_something
-            ;;
-        [Nn]*) 
-            echo "Exiting."
-            exit 0
-            ;;
-        *) 
-            echo "Invalid response. Exiting."
-            exit 1
-            ;;
-    esac
+        case "$answer" in
+            [Yy]*) 
+                do_something
+                ;;
+            [Nn]*) 
+                echo "Exiting."
+                exit 0
+                ;;
+            *) 
+                echo "Invalid response. Exiting."
+                exit 1
+                ;;
+        esac
 
     else
         echo "$username is available. Creating user $username."
         useradd "$username"
+
         if grep -q "^${username}:" /etc/passwd; then
             echo "$username successfully created!"
-        else
-            echo "Failed to create account for username "$username.\nScript failed.\n\nExiting."
 
+        else
+            echo "Failed to create account for username $username."
+            echo "Script failed."
+            echo
+            echo "Exiting."
+            exit 1
+        fi
     fi
 }
 
 
-# 2. Initial configuration of user
+
 
 function select_password {
 
 }
+
+
+choose_username
+
+
+# 2. Initial configuration of user
+
