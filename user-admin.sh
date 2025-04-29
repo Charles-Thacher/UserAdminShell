a64fcf26a82205ee8f87695ed1d53e0461838a08
Charles Thacher <crichardthacher@gmail.com>
2025-04-29 19:42:16 -0400

    first function in user admin

--- user-admin.sh
+++ user-admin.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 # Administrate network users
+# Functions in this source:
+# list_users
 
 
-# Option 1: Changer permissions
+# Option a: List Users
+function list_users {
+    awk -F: '$3 >= 1000 && $3 < 65534 { print $1 }' /etc/passwd
+}
 
 # Option 2: Change password
